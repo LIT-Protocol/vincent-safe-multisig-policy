9a5ae6530d174185526c2cf47fed4e0135d4a767
Wyatt Barnes <me@wyatt.email>
2025-07-14 14:55:24 -1000

    fix: remove old dist files

--- vincent-packages/policies/safe-multisig/dist/lib/helpers/createParametersString.js
+++
@@ -1,5 +0,0 @@
-import { deterministicStringify } from "./deterministicStringify";
-export function createParametersString(toolParams) {
-    console.log("[createParametersString] Creating parameters string for tool params: ", deterministicStringify(toolParams));
-    return deterministicStringify(toolParams);
-}
